0c223720a9750db0621d37dbce88a7833dede65b
DavidUps <davidarribas.ciclos@gmail.com>
2024-02-15 19:48:48 +0100

    rename

---
+++ app/src/main/java/com/davidups/marvel/core/navigation/NavControllerWrapper.kt
@@ -0,0 +1,8 @@
+package com.davidups.marvel.core.navigation
+
+import android.annotation.SuppressLint
+import androidx.navigation.NavHostController
+
+object NavControllerWrapper {
+    var navController: NavHostController? = null
+}
